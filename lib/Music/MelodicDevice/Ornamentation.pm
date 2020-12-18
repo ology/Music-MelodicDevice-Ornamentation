f3476e4f6157e6759bab1f913771826ead094ece
Gene Boggs <gene@ology.net>
2020-12-17 20:40:48 -0800

    Bump version

--- lib/Music/MelodicDevice/Ornamentation.pm
+++ lib/Music/MelodicDevice/Ornamentation.pm
@@ -2,7 +2,7 @@ package Music::MelodicDevice::Ornamentation;
 
 # ABSTRACT: Chromatic and diatonic musical ornamentation
 
-our $VERSION = '0.0101';
+our $VERSION = '0.0200';
 
 use Carp qw(croak);
 use Data::Dumper::Compact qw(ddc);
